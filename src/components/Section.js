8d3bd0b5bc9ee23b0ab33cc09c12b03317529015
glenchiridza <glenchiridz@gmail.com>
2022-02-19 22:18:24 +0200

    section model-s

--- src/components/Section.js
+++ src/components/Section.js
@@ -8,6 +8,23 @@ function Section() {
             <p>Order Online for Touchless Delivery</p>
         </ContentText>
 
+        <ActionButtons>
+            <ButtonGroup>
+                <LeftButton>
+                    Custom Order
+                </LeftButton>
+
+                <RightButton>
+                    Existing Inventory
+                </RightButton>
+
+            </ButtonGroup>
+
+
+            <DownArrow src="/images/down-arrow.svg"/>
+
+        </ActionButtons>    
+
     </Wrap>
   );
 }
@@ -21,9 +38,50 @@ const Wrap = styled.div`
     background-size:cover;
     background-position:center;
     background-repeat:no-repeat;
+    display:flex;
+    flex-direction:column;
+    justify-content:space-between;
+    align-items:center;
 `
 
 const ContentText = styled.div`
     padding-top:14vh;
     text-align:center;
+`
+
+const ButtonGroup = styled.div`
+    display:flex;
+    margin-bottom:20px;
+`
+
+const LeftButton = styled.div`
+    font-size:13px;
+    text-transform:uppercase;
+    opacity:0.85;
+    background:rgba(23,26,33,0.8);
+    height:40px;
+    width:250px;
+    color:#fff;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    border-radius:54px;
+    cursor:pointer;
+    margin:10px;
+
+`
+//inheriting css of the left button
+const RightButton = styled(LeftButton)`
+
+`
+
+const DownArrow = styled.img`
+    margin-top:25px;
+    height:40px;
+    overflow-x:hidden;
+    animation: downArrowAnim infinite 1.6s;
+`
+
+const ActionButtons = styled.div`
+
 `
