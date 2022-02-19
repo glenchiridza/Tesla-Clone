44b8663ab8b88316e40064322b4df6955c9c9661
glenchiridza <glenchiridz@gmail.com>
2022-02-19 23:28:25 +0200

    menu icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import MenuIcon from "@material-ui/icons/Menu";
 
 function Header(){
     return (
@@ -8,6 +9,37 @@ function Header(){
             <a>
                 <img src='images/logo.svg' alt=''/>
             </a>
+            <Menu>
+            
+                    <a href='#model3'>Model 3</a>
+                
+            
+                    <a href='#modelS'>Model Y</a>
+                
+            
+                    <a href='#modelS'>Model S</a>
+                
+            
+                    <a href='#modelS'>Model X</a>
+
+                    <a href='#solar-panel'>Solar Panel</a>
+
+                    <a href='#solar-roof'>Solar Roof</a>
+
+                
+                
+            </Menu>
+
+            <RightMenu>
+                <a href="#">Shop</a>
+                <a href="">Account</a>
+
+                <CustomMenu>
+
+                </CustomMenu>
+
+            </RightMenu>
+
         </Container>
     );
 }
@@ -15,5 +47,39 @@ function Header(){
 export default Header;
 
 const Container = styled.div`
+    min-height:65px;
+    position:fixed;
+    display:flex;
+    align-items:center;
+    padding: 0 20px;
+    top:0;
+    left:0;
+    right:0;
+`
 
+const Menu = styled.div`
+    display:flex;
+    align-items:center;
+    justify-content:center;
+    flex:1;
+    a{
+        font-weight:600;
+        padding:0 10px;
+        flex-wrap:no-wrap;
+    }
+`
+
+const RightMenu = styled.div`
+    display:flex;
+    align-items:center;
+    a{
+    font-weight:600;
+    margin-right:10px;
+    flex-wrap:no-wrap;
+    }
+`
+
+const CustomMenu = styled(MenuIcon)`
+    cursor:pointer;
+    
 `
