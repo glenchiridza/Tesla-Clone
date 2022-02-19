a631d110501d0e1beb8bb22aaf68f492fe5cc474
glenchiridza <glenchiridz@gmail.com>
2022-02-19 22:57:14 +0200

    header start

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
 
 function Header(){
     return (
 
-        <div>
-            header
-        </div>
+        <Container>
+            <a>
+                <img src='images/logo.svg' alt=''/>
+            </a>
+        </Container>
     );
 }
 
 export default Header;
+
+const Container = styled.div`
+
+`
