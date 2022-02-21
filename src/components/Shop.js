05fd8ccdcfa31d696580a1ef6a434c0c7770509e
glenchiridza <glenchiridz@gmail.com>
2022-02-21 15:49:49 +0200

    accessories

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -11,6 +11,32 @@ function Shop() {
             </div>
         </HeadBackImage>
 
+        <BestSeller>
+            <p>Best Seller</p>
+            <div>
+                <img src="images/models.avif" />
+                <h2>Model S Accessories</h2>
+            </div>
+
+            <CenterModel>
+                <ImageLeft>
+                    <img src="images/model3.avif" />
+                </ImageLeft>
+                <ImageRight>
+                    <img src="images/modelx.avif" />
+                </ImageRight>
+                  
+            </CenterModel>
+            
+
+            <div>
+                <img src="images/model_y.avif" />
+                <h2>Model Y Accessories</h2>
+            </div>
+        </BestSeller>
+
+        
+
     </HeadContainer>
   )
 }
@@ -44,5 +70,49 @@ const HeadBackImage = styled.div`
         font-size:22px;
     }
 
+`;
+
+const BestSeller = styled.div`
+    margin:1.5%;
+    p{
+        width:100vw;
+        text-align:start;
+        margin-top:5%;
+        margin-bottom:2%;
+        text-transform:uppercase;
+        font-weight:200;
+    }
+    img{
+        min-height:350px;
+        max-height:350px;
+        width:100vw;
+        object-fit:cover;
+    }
+    h2{
+        font-family:'Times New Roman', sans-serif;
+        position:absolute;
+        top:140vh;
+        right:12vw;
+
+    }
+    
+`
+const CenterModel = styled.div`
+
+    margin:1.5%;
+    display:flex;
+    flex-direction:row;
+    flex-wrap:wrap;
+    width:100vw;
+
+`;
+
+const ImageLeft = styled.div`
+        width:50vw;
+        object-fit:cover;
+        padding:10px;
+`
+
+const ImageRight = styled(ImageLeft)`
 
 `
